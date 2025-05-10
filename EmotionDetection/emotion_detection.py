a782f9a4473a5143ce36cc3bb2f1051d518d6a10
Jeanette Poh <66480377+jeanettepoh@users.noreply.github.com>
2025-05-10 13:35:24 +0800

    update task 3 formatting

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,17 +1,19 @@
 import json
 import requests  # Import the requests library to handle HTTP requests
 
-def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
+def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion detection service
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formatted_response = json.loads(response.text)
+    
     anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
     disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
     fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
     joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
     sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"] 
+    
     emotions_only = {
         'anger': anger_score,
         'disgust': disgust_score,
@@ -19,9 +21,11 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
         'joy': joy_score,
         'sadness': sadness_score
     }
+    
     dominant_emotion = max(emotions_only, key=emotions_only.get)
     emotions_dict = {
         **emotions_only,
         'dominant_emotion': dominant_emotion
     }
+    
     return emotions_dict
